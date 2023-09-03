c1c02fda16a9567fca30390b096cc856c75c7c78
MesKaNoVa <abumettleq@iut-dhaka.edu>
2023-09-03 22:47:40 +0600

    add: Renamed some stuffs

--- lib/login.dart
+++ lib/login.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: LoginPage(),
       ),
@@ -20,69 +21,79 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Color(0xFFffd8ef), // Use the CSS color variable
+        gradient: LinearGradient(
+          colors: [Color(0xFFffd8ef), Colors.white],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
       ),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
-              'rectangle-1.png', // Replace with your image asset
-              width: 600, // Set width and height as needed
-              height: 1024,
+              'rectangle-1.png',
+              width: 300,
+              height: 500,
             ),
+            SizedBox(height: 30),
             Image.asset(
-              'chandelier-with-green-round-lampshade@2x.png', // Replace with your image asset
-              width: 174,
-              height: 276,
+              'lampshade.png',
+              width: 100,
+              height: 160,
               fit: BoxFit.cover,
             ),
+            SizedBox(height: 10),
             Image.asset(
-              'rectangle@2x.png', // Replace with your image asset
-              width: 380,
-              height: 411,
+              'rectangle.png',
+              width: 200,
+              height: 220,
               fit: BoxFit.cover,
             ),
-            SizedBox(height: 3), // Replace the border with a SizedBox
+            SizedBox(height: 30),
             Container(
-              width: 486,
+              width: 300,
               height: 3,
               decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: Color(0xFF8e0467), // Use the CSS color variable
-                    width: 3,
-                  ),
+                gradient: LinearGradient(
+                  colors: [Color(0xFF8e0467), Colors.transparent],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
                 ),
               ),
             ),
-            SizedBox(height: 119), // Adjust spacing as needed
+            SizedBox(height: 30),
             Text(
               'Username',
               style: TextStyle(
-                fontFamily: 'Roboto', // Use the CSS font variable
+                fontFamily: 'Roboto',
+                fontSize: 18,
               ),
             ),
+            SizedBox(height: 10),
             Text(
               'Password',
               style: TextStyle(
                 fontFamily: 'Roboto',
+                fontSize: 18,
               ),
             ),
+            SizedBox(height: 30),
             Text(
               'WELCOME',
               style: TextStyle(
-                fontFamily: 'Risque', // Use the CSS font variable
-                fontSize: 72,
+                fontFamily: 'Risque',
+                fontSize: 48,
+                color: Color(0xFF8e0467),
               ),
             ),
+            SizedBox(height: 20),
             Container(
-              margin: EdgeInsets.only(top: 44),
-              width: 483,
-              height: 76,
+              width: 300,
+              height: 50,
               decoration: BoxDecoration(
                 color: Color(0xFF8e0467),
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(25),
               ),
               child: Center(
                 child: Text(
@@ -94,11 +105,11 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 48), // Adjust spacing as needed
+            SizedBox(height: 20),
             Image.asset(
-              'assets/avatar-with-man-in-green-shirt-and-orange-hat@2x.png', // Replace with your image asset
-              width: 350,
-              height: 128,
+              'avatar.png',
+              width: 200,
+              height: 72,
               fit: BoxFit.cover,
             ),
           ],
