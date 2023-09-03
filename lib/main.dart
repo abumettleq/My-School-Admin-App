f1fa948dc2a401cc6c7d225ca9923c95f096e43e
Mohammed Motlaq <mohammedmotlaq87@gmail.com>
2023-09-03 21:19:07 +0300

    add: login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_school_admin_app/firebase_options.dart';
-
 import 'screens/login.dart';
 
 void main() async {
@@ -16,12 +16,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
+    return ScreenUtilInit(
+      designSize:const Size(1600, 1024),
+        minTextAdapt: true,
+        splitScreenMode: true,
+      builder: (context,child) {
+        return MaterialApp(
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: child,
+          debugShowCheckedModeBanner: false,
+        );
+      },
+      child: const LoginPage(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,104 +1,203 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFFffd8ef), Colors.white],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'rectangle-1.png',
-              width: 300,
-              height: 500,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: 1600.w,
+            height: 1024.h,
+            alignment: Alignment.topRight,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFFffd8ef), Colors.white],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
             ),
-            const SizedBox(height: 30),
-            Image.asset(
-              'lampshade.png',
-              width: 100,
-              height: 160,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 10),
-            Image.asset(
-              'rectangle.png',
-              width: 200,
-              height: 220,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 30),
-            Container(
-              width: 300,
-              height: 3,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFF8e0467), Colors.transparent],
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset(
+                  'rectangle-1.png',
+                  width: 694.59.w,
+                  height: 1024.h,
+                  fit: BoxFit.fill,
                 ),
-              ),
+              ],
             ),
-            const SizedBox(height: 30),
-            const Text(
-              'Username',
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Password',
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              'WELCOME',
-              style: TextStyle(
-                fontFamily: 'Risque',
-                fontSize: 48,
-                color: Color(0xFF8e0467),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: 300,
-              height: 50,
-              decoration: BoxDecoration(
-                color: const Color(0xFF8e0467),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child:const  Center(
-                child: Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    color: Color(0xFFffd8ef),
-                    fontSize: 20,
+          ),
+          SizedBox(
+            width: 1600.w,
+            height: 1024.h,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 700.w,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("avatar.png",width: 128.h,height: 128.h,fit: BoxFit.cover,),
+                      SizedBox(height: 30.h,),
+                      Text(
+                        "WELCOME",
+                        style: TextStyle(
+                          color:const Color.fromRGBO(142, 4, 103, 1.0),
+                          fontSize: 72.sp,
+                          fontFamily: 'risque'
+                        ),
+                      ),
+                      SizedBox(height: 40.h,),
+                      SizedBox(
+                        width: 480.w,
+                        height: 80.h,
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                              labelText: 'User Name',
+                              labelStyle: TextStyle(
+                                  color: const Color.fromRGBO(142, 4, 103, 1),
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w500
+                              ),
+                              hintText: 'Enter your User Name',
+                              hintStyle:TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.w400
+                              ),
+                              contentPadding: EdgeInsets.symmetric(horizontal: 10.w,vertical: 10.h),
+                              fillColor: const Color.fromRGBO(255, 216, 239, 0.7),
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:const BorderSide(
+                                    style: BorderStyle.solid,
+                                    color: Color.fromRGBO(142, 4, 103, 1)
+                                ),
+                                borderRadius: BorderRadius.circular(16.r),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:const BorderSide(
+                                    style: BorderStyle.solid,
+                                    color: Color.fromRGBO(142, 4, 103, 1),
+                                    width: 1.5
+                                ),
+                                borderRadius: BorderRadius.circular(16.r),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide:const BorderSide(
+                                  style: BorderStyle.solid,
+                                ),
+                                borderRadius: BorderRadius.circular(16.r),
+                              )
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50.h,),
+                      SizedBox(
+                        width: 480.w,
+                        height: 80.h,
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.next,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                  color: const Color.fromRGBO(142, 4, 103, 1),
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w500
+                              ),
+                              hintText: 'Enter your Password',
+                              hintStyle:TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.w400
+                              ),
+                              contentPadding: EdgeInsets.symmetric(horizontal: 10.w,vertical: 10.h),
+                              fillColor: const Color.fromRGBO(255, 216, 239, 0.7),
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:const BorderSide(
+                                    style: BorderStyle.solid,
+                                    color: Color.fromRGBO(142, 4, 103, 1)
+                                ),
+                                borderRadius: BorderRadius.circular(16.r),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:const BorderSide(
+                                    style: BorderStyle.solid,
+                                    color: Color.fromRGBO(142, 4, 103, 1),
+                                    width: 1.5
+                                ),
+                                borderRadius: BorderRadius.circular(16.r),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide:const BorderSide(
+                                  style: BorderStyle.solid,
+                                ),
+                                borderRadius: BorderRadius.circular(16.r),
+                              )
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50.h,),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(142, 4, 103, 1),
+                          shape: const StadiumBorder(),
+                          foregroundColor: const Color.fromRGBO(255, 216, 239, 0.7)
+                        ),
+                          onPressed: (){},
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 100.w,vertical: 15.h),
+                            child: Text(
+                                "LOGIN",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 26.sp,
+                                  fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                      )
+                    ],
                   ),
                 ),
-              ),
+                const Spacer(),
+                SizedBox(
+                  width: 500.w,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset("lampshade.png",width: 174.w,height: 276.h,),
+                        ],
+                      ),
+                      SizedBox(height: 41.h,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("rectangle.png",width: 380.w,height: 411.h,),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+
+              ],
             ),
-            const SizedBox(height: 20),
-            Image.asset(
-              'avatar.png',
-              width: 200,
-              height: 72,
-              fit: BoxFit.cover,
-            ),
-          ],
-        ),
+
+          )
+
+        ],
       ),
     );
   }
