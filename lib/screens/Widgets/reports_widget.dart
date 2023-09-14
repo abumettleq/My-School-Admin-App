17e9814a5ee005f8542cfcdd122386c9e95ccbc9
Mohammed Motlaq <mohammedmotlaq87@gmail.com>
2023-09-15 00:02:15 +0300

    add: report widget

--- lib/screens/Widgets/reports_widget.dart
+++ lib/screens/Widgets/reports_widget.dart
@@ -13,6 +13,77 @@ class ReportsWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(16.r),
         color: Colors.white,
       ),
+      child: ListView.builder(
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index){
+          return Container(
+            width: 1200.w,
+            height: 210.h,
+            margin: EdgeInsets.symmetric(horizontal: 20.w,vertical: 10.h),
+            padding: EdgeInsets.all(10.w),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.r),
+              color: const Color.fromRGBO(217, 217, 217, 1.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Alaa Mettleq",
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  "123456789",
+                  style: TextStyle(
+                      fontSize: 15.sp,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  width: 1280.w,
+                  height: 90.h,
+                  child: Text(
+                    "Hello World " * 60 ,
+                    style: TextStyle(
+                        fontSize: 15.sp,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.h,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "mettleq@gmail.com",
+                      style: TextStyle(
+                          fontSize: 15.sp,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "Sep 1, 2023",
+                      style: TextStyle(
+                          fontSize: 15.sp,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        }
+      ),
 
     );
   }
